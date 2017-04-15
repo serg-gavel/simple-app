d9c3e1346def34b692705dab7639df0bc089f6e7
Serg <gavelyuk.sergey.13@gmail.com>
2017-04-15 23:06:11 +0300

    first commit

---
+++ src/game.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Board from './board';
+
+
+export default class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{'status'}</div>
+                    <ol>{/*todo*/}</ol>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './game';
+import './styles/index.css';
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
