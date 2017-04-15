9498cfe1bb7fadabc1e19b820b83de4dc1d52d37
Serg <gavelyuk.sergey.13@gmail.com>
2017-04-16 00:28:54 +0300

    upd

--- src/game.js
+++ src/game.js
@@ -3,14 +3,39 @@ import Board from './board';
 
 
 export default class Game extends React.Component {
+    constructor(){
+        super();
+
+        this.state ={
+            xIsNext: true,
+            stepNumber: 0,
+            history: [{
+                squares: Array(9).fill(null)
+            }]
+        };
+    }
+
+    handleClick(i){
+        console.log('handleClick', i);
+    }
+
     render() {
+        const {xIsNext, stepNumber, history} = this.state;
+        const current = history[stepNumber];
+
+        const status = 'Next player is: ' + (xIsNext ? 'X' : '0');
+
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{'status'}</div>
+                    <div>{status}</div>
                     <ol>{/*todo*/}</ol>
                 </div>
             </div>
